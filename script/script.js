edc35ffd1a07fc47f654d327a006696db9a5fbb1
ZVS <zvsargsyan@gmail.com>
2019-08-13 21:25:38 +0400

    adding some sections

--- script/script.js
+++ script/script.js
@@ -1,6 +1,17 @@
-function getStarted(){
+function getStarted() {
     console.log('start')
 }
-function getInTouch(){
+function getInTouch() {
     console.log('ready')
 }
+
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
