081033e53c8a2db18b40c510e1535116a06d8b6b
ZVS <zvsargsyan@gmail.com>
2019-08-17 01:20:49 +0400

    update form

--- script/script.js
+++ script/script.js
@@ -71,7 +71,6 @@ function finalFormPage() {
     });
 }
 function finish() {
-    console.log(document.querySelector('form'))
     document.querySelector('.form-swiper-container').classList.add('done')
     document.querySelector('.form-swiper-container').innerHTML = `
             <img src="./image/done.png">
